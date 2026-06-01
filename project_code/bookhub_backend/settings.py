9d557bf879d06eb674d370001afce8f8de94f06a
Bipin Yadav <bipin@example.com>
2026-06-01 10:49:03 +0530

    Fix Vercel bad request - set ALLOWED_HOSTS to * and DEBUG to True

--- project_code/bookhub_backend/settings.py
+++ project_code/bookhub_backend/settings.py
@@ -13,8 +13,8 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,librarymanagement-main-3.vercel.app,*.vercel.app').split(',')
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
